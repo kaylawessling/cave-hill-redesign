2edd699138f4af8d2afb90496e6740b53bd1313c
kaylawessling <kwessling05@gmail.com>
2018-11-05 20:12:39 -0500

    stylized subpages

---
+++ js/ch-redesign.js
@@ -0,0 +1,4 @@
+function Submit() {
+   alert ("Thank you for your submission! Someone will be in contact with you shortly.");
+   document.write ("Thank you for your submission! Someone will be in contact with you shortly.");
+}
