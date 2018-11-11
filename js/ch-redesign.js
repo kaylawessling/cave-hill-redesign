bade915885c85043ccf2a3b6ef67e6a039912ccc
kaylawessling <kwessling05@gmail.com>
2018-11-11 18:37:45 -0500

    added JavaScript pop-up confirmation on Options, linked JS file to all other pages

--- js/ch-redesign.js
+++ js/ch-redesign.js
@@ -1,10 +1,6 @@
-// function Submit() {
-//    alert ("Thank you for your submission! Someone will be in contact with you shortly.");
-//    document.write ("Thank you for your submission! Someone will be in contact with you shortly.");
-// }
-
-const Submit = document.querySelector('.Submit');
-
-$('.Submit').click(function() {
-  alert("Thank you for your submission! Someone will be in contact with you shortly.");
+// Burial Options submit button pop-up confirmation alert
+$(document).ready(function(){
+  $('#submit-button').click(function() {
+    alert("Thank you for your submission! Someone will be in contact with you shortly.");
+  });
 });
