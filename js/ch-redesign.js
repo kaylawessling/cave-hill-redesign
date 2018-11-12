45ff20450531628c0576b620ef341bd06a5aa95d
kaylawessling <kwessling05@gmail.com>
2018-11-11 19:44:51 -0500

    styalized submit from on options page, added images to folder

--- js/ch-redesign.js
+++ js/ch-redesign.js
@@ -1,6 +1,6 @@
 // Burial Options submit button pop-up confirmation alert
 $(document).ready(function(){
-  $('#submit-button').click(function() {
+  $('.submit-button').click(function() {
     alert("Thank you for your submission! Someone will be in contact with you shortly.");
   });
 });
