240648e8be37a44fa85ed592171887dcbf1dadc1
kaylawessling <kwessling05@gmail.com>
2018-11-15 23:31:14 -0500

    modified responsiveness of navigation bar, deleted memorials page

--- js/ch-redesign.js
+++ js/ch-redesign.js
@@ -1,6 +1,6 @@
-// Burial Options submit button pop-up confirmation alert
+// Confirmation pop-up when 'Submit' is clicked on Burial Options page
 $(document).ready(function(){
-  $('.submit-button').click(function() {
+  $('#submit-button').click(function() {
     alert("Thank you for your submission! Someone will be in contact with you shortly.");
   });
 });
